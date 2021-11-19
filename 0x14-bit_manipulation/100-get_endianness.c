228c764e9aa04efd5107f5d040a570a64375fa20
Cecales <cesar.caleroes@gmail.com>
2021-11-19 15:07:49 -0500

    Upload point number six

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks if a machine is little or big endian
+ * Return: 0 for big, 1 for little
+ */
+
+int get_endianness(void)
+{
+	int i;
+	char *test;
+
+	i = 1;
+	test = (char *)&i;
+	return ((int)test[0]);
+}
